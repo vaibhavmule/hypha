d7b368c5538a5ec3eea854bc680b6dc4f965f820
Fredrik Jonsson <frjo@xdeb.org>
2020-03-26 11:49:45 +0100

    Set target blanks on all external links in Wagtail editor. Make target blank default for all link in TineMCE editor.

--- hypha/public/utils/wagtail_hooks.py
+++ hypha/public/utils/wagtail_hooks.py
@@ -1,10 +1,13 @@
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.html import escape
+
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin,
     ModelAdminGroup,
     modeladmin_register,
 )
 from wagtail.core import hooks
+from wagtail.core.rich_text import LinkHandler
 from wagtailcache.cache import clear_cache
 
 from hypha.public.news.models import NewsType
@@ -42,3 +45,21 @@ def editor_css():
 def clear_wagtailcache(request, page):
     if page.live:
         clear_cache()
+
+
+class NewWindowExternalLinkHandler(LinkHandler):
+    # This specifies to do this override for external links only.
+    # Other identifiers are available for other types of links.
+    identifier = 'external'
+
+    @classmethod
+    def expand_db_attributes(cls, attrs):
+        href = attrs["href"]
+        # Let's add the target attr, and also rel="noopener" + noreferrer fallback.
+        # See https://github.com/whatwg/html/issues/4078.
+        return '<a href="%s" target="_blank" rel="noopener noreferrer">' % escape(href)
+
+
+@hooks.register('register_rich_text_features')
+def register_external_link(features):
+    features.register_link_type(NewWindowExternalLinkHandler)
